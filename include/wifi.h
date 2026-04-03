45b009d1dd79118a29b530388e73ca9640bb0a48
stellaaa <tttotaa42@gmail.com>
2026-04-03 11:15:16 +0300

    simplify

--- include/wifi.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Connect to WiFi in station mode.
-// Blocks until connected or times out.
-// Returns 1 on success, 0 on failure.
-int  wifi_connect(const char *ssid, const char *password);
-void wifi_get_ip(char *buf, int buf_len); // fills buf with "192.168.x.x"
